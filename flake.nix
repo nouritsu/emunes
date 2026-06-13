23b8b759b8537512c43f28cbada4cce010dbc202
Aneesh Bhave <aneesh1701@gmail.com>
2026-06-13 17:12:09 +0200

    feat(flake): add git-hooks - run `cargo fmt --check` pre-commit - run `cargo test` pre-commit - ignore generated hooks.yaml

--- flake.nix
+++ flake.nix
@@ -8,9 +8,13 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, git-hooks }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -21,9 +25,33 @@
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" "rust-analyzer" ];
         };
+
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            cargo-fmt = {
+              enable = true;
+              name = "cargo fmt --check";
+              entry = "${rustToolchain}/bin/cargo fmt --all -- --check";
+              files = "\\.rs$";
+              pass_filenames = false;
+            };
+            cargo-test = {
+              enable = true;
+              name = "cargo test";
+              entry = "${rustToolchain}/bin/cargo test";
+              files = "\\.rs$";
+              pass_filenames = false;
+            };
+          };
+        };
       in
       {
+        checks.pre-commit-check = pre-commit-check;
+
         devShells.default = pkgs.mkShell {
+          inherit (pre-commit-check) shellHook;
+          buildInputs = pre-commit-check.enabledPackages;
           packages = [ rustToolchain ];
         };
       });
